5b9c35a7d6bb639f254b9fd064bb8c2829a6ba65
Erick Zanardo <erickzanardoo@gmail.com>
2018-12-18 09:44:50 -0200

    Scripts

--- ide/src/actions/fs.js
+++ ide/src/actions/fs.js
@@ -2,8 +2,17 @@ const fs = window.require("fs");
 const path = window.require("path");
 
 export const readProject = projectFilePath => dispatch => {
-  const projectPath = path.basename(projectFilePath);
+  const projectPath = path.dirname(projectFilePath);
   const projectFile = JSON.parse(fs.readFileSync(projectFilePath, "utf8"));
+
+  fs.readdir(path.join(projectPath, "sources"), (err, files) => {
+    files.forEach(fileName => {
+      dispatch({
+        type: "READ_SCRIPT",
+        payload: { fileName },
+      })
+    });
+  })
 
   dispatch({
     type: "READ_PROJECT",
--- ide/src/reducers/scripts.js
+++ ide/src/reducers/scripts.js
@@ -3,5 +3,16 @@ const initialState = {
 }
 
 export default (state = initialState, action) => {
+  switch(state.type) {
+    case "READ_SCRIPT": {
+      const script = {
+        fileName: action.payload.fileName,
+        loaded: false,
+        modified: false
+      };
+
+      return { ...state, scripts: [ ...state.scripts, script ] }
+    }
+  }
   return state;
 }
