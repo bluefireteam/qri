973c980f98ec1cad5cc1c32578c635702b8c4344
Erick Zanardo <erickzanardoo@gmail.com>
2018-12-21 11:13:28 -0200

    Adding ace editor

--- ide/src/components/ScriptEditor/index.js
+++ ide/src/components/ScriptEditor/index.js
@@ -1,9 +1,25 @@
 import React from "react";
+import brace from "brace";
+import AceEditor from "react-ace";
+
+import "brace/mode/javascript";
+import "brace/theme/monokai";
 
 import "./styles.css";
 
 export default ({ fileName, value }) => (
   <div className="script-editor-container">
-    { value }
+  <AceEditor
+    mode="javascript"
+    theme="monokai"
+    value={value}
+    width="600px"
+    height="600px"
+    onChange={value => {
+      console.log(value)
+		}}
+    name={`AceEditor-${fileName}`}
+    editorProps={{}}
+  />
   </div>
 )
