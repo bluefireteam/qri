9dd38b9094dc79d76dbb8f067daae4cf13593150
Erick Zanardo <erickzanardoo@gmail.com>
2018-12-21 11:19:06 -0200

    Editor full size

--- ide/src/components/ScriptEditor/index.js
+++ ide/src/components/ScriptEditor/index.js
@@ -8,18 +8,18 @@ import "brace/theme/monokai";
 import "./styles.css";
 
 export default ({ fileName, value }) => (
-  <div className="script-editor-container">
-  <AceEditor
-    mode="javascript"
-    theme="monokai"
-    value={value}
-    width="600px"
-    height="600px"
-    onChange={value => {
-      console.log(value)
-		}}
-    name={`AceEditor-${fileName}`}
-    editorProps={{}}
-  />
+  <div className="script-editor-container full-size">
+    <AceEditor
+      mode="javascript"
+      theme="monokai"
+      value={value}
+      width="100%"
+      height="100%"
+      onChange={value => {
+        console.log(value)
+	  	}}
+      name={`AceEditor-${fileName}`}
+      editorProps={{}}
+    />
   </div>
 )
